e2e1551eda7a01bcc103b5d16cc39e6d44aeff42
Asritha <asrithakondapaneni@gmail.com>
2021-06-23 17:32:59 +0530

    springboot-logging

---
+++ Springboot_Logging/src/main/java/com/vm/logging/Springbootlogging.java
@@ -0,0 +1,13 @@
+package com.vm.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootlogging {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootlogging.class, args);
+	}
+
+}
---
+++ Springboot_Logging/src/test/java/com/vm/logging/SpringbootloggingTests.java
@@ -0,0 +1,13 @@
+package com.vm.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootloggingTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
